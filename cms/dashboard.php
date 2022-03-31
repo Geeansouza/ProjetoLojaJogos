03e9cf799d8facc95874eb6f51c756c839d9a446
LEONARDO DE NOVAES VIVI <21276367@CFP127ALUNO.LOCAL>
2022-03-31 16:53:54 -0300

    .

---
+++ cms/dashboard.php
@@ -0,0 +1,48 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/dash.css"> 
+    <title>.</title>
+</head>
+<body>
+   <main>
+       <div id=caixa>
+           <div id="topo">
+                <div id="titulo">
+                    <p class="cms">CMS</p>
+                    <p class="tg">Torrent Games</p>
+                    <p class="gerenciamento">Gerenciamento de Conteúdo do Site</p>
+                </div>
+                <div id="img">
+                    <img id="logo"src="img/Tg.png" alt="">
+
+                </div>
+           </div>
+           <div id="atalhos">
+               <div id="buttons">  
+                   <button id=adm>
+                        <img id="admin" src="img/admin.jpg" alt="">
+                    </button>
+                    <button id=categorias>
+                        <img id="admin" src="img/categorias.png" alt="">
+                    </button>
+                    <button id=contatos>
+                        <img id="admin" src="img/contato.jpg" alt="">
+                    </button>
+                    <button id=usuarios>
+                        <img id="admin" src="img/user.png" alt="">
+                    </button>
+                </div>
+                <Div id=sair>
+                    <button id="salir">
+                        <img id="imgs"src="img/Vector.png" alt="">
+                    </button>
+                </Div>
+            </div>
+           <div id="sessao">
+           </div>
+       </div>
+   </main>
+</body>
+</html>
